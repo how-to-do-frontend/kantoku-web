02cac7adbc4985eaa1803f52057a143c917866fc
tree <fysixdev@gmail.com>
2023-05-08 11:55:47 +0000

    fix search, improve badges, bump ver to 0.7.2

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from objects import glob
 
 app = Quart(__name__)
 
-version = Version(1, 3, 0)
+version = Version(0, 7, 2)
 
 # used to secure session data.
 # we recommend using a long randomly generated ascii string.
